8483b312229994883f7a237b55a9d0e2b82b71a2
Karl Erik Holter <karl0erik@gmail.com>
2018-06-08 15:20:00 +0200

    Added loading of boundary markers. Demonstrated setting BCs

--- benchmark2/benchmark2.py
+++ benchmark2/benchmark2.py
@@ -1,43 +1,48 @@
 import matplotlib.pyplot as plt
-
-from geometry_utils import load_geometry_from_h5
-
-#from geometry import *
-#from dolfin import *
-
 from dolfin import *
+
+def load_ellipsoid_mesh_markers_numbering():
+    """Returns triple of Mesh, MeshFunction and dict of marking numbers.
+    The MeshFunction numbers domains according to the numbering dict."""
+    comm = mpi_comm_world()
+    hdf5file = dolfin.HDF5File(dolfin.mpi_comm_world(), "simple_ellipsoid.h5", "r")
+
+    mesh = Mesh(comm)
+    hdf5file.read(mesh, "/geometry/mesh", True)
+
+    mf = MeshFunction("size_t", mesh, 2, mesh.domains()) # 2 = dimension of entities (facets)
+    hdf5file.read(mf, "/geometry/mesh/meshfunction_2")
+
+    # hack to make function look nicer in Paraview
+    # as-is interior facets have a huge int which makes other
+    # values look the same, but irrelevant for BCs
+    big_num = max(mf.array())
+    mf.array()[mf.array() == big_num] = 0
+    
+    File("domain_markers.pvd") << mf # may be opened with paraview
+
+    numbering = {
+        "BASE": 10,
+        "ENDO": 30,
+        "EPI": 40
+    }
+
+    return mesh, mf, numbering
+                 
 
 # Adjust log level
 set_log_level(PROGRESS)
 parameters["form_compiler"]["cpp_optimize"] = True
 
-geo = load_geometry_from_h5('simple_ellipsoid.h5')
+mesh, boundary_markers, numbering = load_ellipsoid_mesh_markers_numbering()
 
-mesh = geo.mesh
-
-V = VectorFunctionSpace(geo.mesh,'P',1)
-
-new_ffun = MeshFunction("size_t", mesh, 2, mesh.domains())
-print set(new_ffun.array())
-
-exit()
-
-"""
-base =  CompiledSubDomain("near(x[0], -5) && on_boundary")
-endo = CompiledSubDomain("pow(x[1]/rs,2.0)+pow(x[2]/rs,2.0)+pow(x[0]/rl,2.0) < tol && on_boundary",rs=7,rl=17,tol=1e-3)
-
-boundary_markers = MeshFunction("size_t", mesh,mesh.topology().dim() - 1)
-boundary_markers.set_all(0)
-base.mark(boundary_markers, 1)
-endo.mark(boundary_markers, 2)
-"""
-
+V = VectorFunctionSpace(mesh,'P',1)
 
 # Redefine boundary measure
 ds = Measure('ds',domain=mesh,subdomain_data=boundary_markers)
 
 clamp = Constant((0.0, 0.0, 0.0))
-bc = DirichletBC(V, clamp, base)
+bc = DirichletBC(V, clamp, boundary_markers, numbering["BASE"])
 bcs = [bc]
 
 # Define solution u and test function v
@@ -63,16 +68,11 @@ W = lmbda/2*(tr(E)**2) + mu*tr(E*E)
 
 S = diff(W, E) # the second Piola-Kirchoff stress tensor
 P = F*S        # the first Piola-Kirchoff stress tensor
-
-
-#endo = geo.markers['ENDOPT'][0]
-#print endo
 p_endo = 1.0e2
+
 # Define nonlinear problem
-
-
-N = FacetNormal(geo.mesh)
-Gext = p_endo * inner(v, cofac(F)*N) * ds(2) #_bottom
+N = FacetNormal(mesh)
+Gext = p_endo * inner(v, cofac(F)*N) * ds(numbering["ENDO"]) #_bottom
 R = inner(P,grad(v))*dx + Gext #dot(T,v)*ds(2)
 #J = derivative(R,u,du)
 
@@ -82,8 +82,4 @@ solve(R==0,u,bcs)
 file = File("displacement.pvd")
 file << u
 
-
-#print ds.subdomain_data()
-#fixed_base = DirichletBC(V,Constant(0,0,0),
-#plt.show()
 
